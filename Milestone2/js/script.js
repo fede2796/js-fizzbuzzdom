2e91adec708f986a816121098445171b0b67de9f
fede2796 <fede27396@gmail.com>
2022-06-07 16:16:03 +0200

    Fine Mainston 2

---
+++ Milestone2/js/script.js
@@ -0,0 +1,23 @@
+const mainlist = document.querySelector('.list');
+ // Scrivo i numeri da 1 a 100
+ for(let i=1; i <= 100; i++){
+    let number;
+    number = i ; 
+    //Se il numero è un multiplo di 3 prende il valore di Fizz
+    //Se il numero è un multiplo di 5 prende il valore di Buzz
+    //Se il numero è un multiplo di entrambi prende il Valore di Fizzbuzz
+    if(i % 3 === 0){
+        number = 'Fizz';
+    }
+    else if(i % 5 === 0){
+        number = 'Buzz';
+    }
+    if(i % 3 === 0 && i % 5 === 0){
+        number = 'FizzBuzz'
+    }
+    mainlist.innerHTML += `<li> ${number} </li>` 
+    console.log(number)
+    
+}
+
+
